4bf2b626364c817c3ee984144c13896551464768
jj-matos <jj-matos@users.noreply.github.com>
2022-01-03 14:58:04 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,9 @@ const App = () => {
         label="Search"
         value={searchTerm}
         onInputChange={handleSearch}
-      />
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
@@ -68,10 +70,11 @@ const InputWithLabel = ({
   label, 
   value, 
   type = 'text',
-  onInputChange
+  onInputChange,
+  children
 }) => (
   <React.Fragment>
-    <label htmlFor={id}>{label}</label>
+    <label htmlFor={id}>{children}</label>
     &nbsp;
     <input 
       id={id}
