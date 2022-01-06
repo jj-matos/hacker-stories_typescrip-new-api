e5de95f838f8301eb68862b1cdf1a5bb648e9d68
jj-matos <jj-matos@users.noreply.github.com>
2022-01-06 11:29:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,13 +31,28 @@ const App = () => {
       objectID: 1,
     },
   ];
-  
-  const [searchTerm, setSearchTerm] = useSemiPersistentState(
-    'search',
-    'React'
+
+  const getAsyncStories = () =>
+    new Promise((resolve) =>
+      setTimeout(
+        () => resolve({ data: { stories: initialStories } }),
+        2000
+    )
   );
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+      'search',
+      'React'
+    );
+
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
+
 
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
