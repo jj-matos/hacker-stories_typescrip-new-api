48e6fd3fc3b1e61cadc4c04918722257c1978e9c
jj-matos <jj-matos@users.noreply.github.com>
2022-01-07 11:29:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,20 @@ const useSemiPersistentState = (key, initialState) => {
 
   return [value, setValue];
 };
-  
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+    default:
+      throw new Error();
+  }
+};
+
 const App = () => {
   const initialStories = [
     {
@@ -45,7 +58,11 @@ const App = () => {
       'React'
     );
 
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
+
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
@@ -53,7 +70,10 @@ const App = () => {
     setIsLoading(true);
 
     getAsyncStories().then(result => {
-      setStories(result.data.stories);
+      dispatchStories({
+        type: 'SET_STORIES',
+        payload: result.data.stories,
+      });
       setIsLoading(false);
     })
     .catch(() => setIsError(true));
@@ -61,13 +81,11 @@ const App = () => {
 
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-
-    setStories(newStories);
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
   };
-
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
